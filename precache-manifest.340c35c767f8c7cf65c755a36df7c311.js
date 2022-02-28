6126e1a21307e94bf9d252e8845887e40d91b4de
ahmedabosaif <ahmed.t.abosaif@gmail.com>
2022-02-28 18:23:32 +0200

    Updates

---
+++ precache-manifest.340c35c767f8c7cf65c755a36df7c311.js
@@ -0,0 +1,338 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "985d3f6307e6a793e6b38cdb8e5feebd",
+    "url": "/hrportal/index.html"
+  },
+  {
+    "revision": "8be51abcb44a0fcf8b3e",
+    "url": "/hrportal/static/css/1.819807a6.chunk.css"
+  },
+  {
+    "revision": "740284c519e295365b70",
+    "url": "/hrportal/static/css/15.819807a6.chunk.css"
+  },
+  {
+    "revision": "7aee3cd42d7767cc664b",
+    "url": "/hrportal/static/css/16.455ef3d9.chunk.css"
+  },
+  {
+    "revision": "81c05ecb9981c03e002f",
+    "url": "/hrportal/static/css/17.819807a6.chunk.css"
+  },
+  {
+    "revision": "031553198c0e78bb51ae",
+    "url": "/hrportal/static/css/27.455ef3d9.chunk.css"
+  },
+  {
+    "revision": "e6ca01f5b07c251d61f6",
+    "url": "/hrportal/static/css/28.455ef3d9.chunk.css"
+  },
+  {
+    "revision": "1a567a0cc594fc65f18e",
+    "url": "/hrportal/static/css/29.455ef3d9.chunk.css"
+  },
+  {
+    "revision": "3c892f9f80c22cec7f94",
+    "url": "/hrportal/static/css/30.455ef3d9.chunk.css"
+  },
+  {
+    "revision": "2124359eb04965515468",
+    "url": "/hrportal/static/css/31.455ef3d9.chunk.css"
+  },
+  {
+    "revision": "b1ef3f17b07c53db96ac",
+    "url": "/hrportal/static/css/33.455ef3d9.chunk.css"
+  },
+  {
+    "revision": "2ad8e2b3973c967818e3",
+    "url": "/hrportal/static/css/34.455ef3d9.chunk.css"
+  },
+  {
+    "revision": "8d7e65b3743789313d4b",
+    "url": "/hrportal/static/css/35.455ef3d9.chunk.css"
+  },
+  {
+    "revision": "e97f2a3bc71e576048ce",
+    "url": "/hrportal/static/css/36.455ef3d9.chunk.css"
+  },
+  {
+    "revision": "391167dc8d7f137133f5",
+    "url": "/hrportal/static/css/4.819807a6.chunk.css"
+  },
+  {
+    "revision": "901f63a044e614abc817",
+    "url": "/hrportal/static/css/7.99190d76.chunk.css"
+  },
+  {
+    "revision": "58250c0771a1e35051d7",
+    "url": "/hrportal/static/css/main.cb36501d.chunk.css"
+  },
+  {
+    "revision": "2dd04bb390f99ce9f96b",
+    "url": "/hrportal/static/js/0.8751ca83.chunk.js"
+  },
+  {
+    "revision": "8be51abcb44a0fcf8b3e",
+    "url": "/hrportal/static/js/1.f20b3fe8.chunk.js"
+  },
+  {
+    "revision": "43fd2f7371cf60e44e94",
+    "url": "/hrportal/static/js/10.9ac33d7d.chunk.js"
+  },
+  {
+    "revision": "8f4da82db82d20bfbbe6",
+    "url": "/hrportal/static/js/11.8d792be3.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "/hrportal/static/js/11.8d792be3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68276a7371f743a3f5db",
+    "url": "/hrportal/static/js/12.d3351b78.chunk.js"
+  },
+  {
+    "revision": "a4255024c2a5238b3e05",
+    "url": "/hrportal/static/js/13.bec40d77.chunk.js"
+  },
+  {
+    "revision": "e7dec7223dc460fd7fee",
+    "url": "/hrportal/static/js/14.bcb8c7aa.chunk.js"
+  },
+  {
+    "revision": "740284c519e295365b70",
+    "url": "/hrportal/static/js/15.7b92117b.chunk.js"
+  },
+  {
+    "revision": "7aee3cd42d7767cc664b",
+    "url": "/hrportal/static/js/16.f9b2514d.chunk.js"
+  },
+  {
+    "revision": "81c05ecb9981c03e002f",
+    "url": "/hrportal/static/js/17.5babeeb0.chunk.js"
+  },
+  {
+    "revision": "d111c37fdce7a154c563",
+    "url": "/hrportal/static/js/18.0f9c1e53.chunk.js"
+  },
+  {
+    "revision": "9542640ca141ccfdbc16",
+    "url": "/hrportal/static/js/19.0cf20ade.chunk.js"
+  },
+  {
+    "revision": "ed52138e9eb255fb0284",
+    "url": "/hrportal/static/js/2.24eaea32.chunk.js"
+  },
+  {
+    "revision": "ebb0e632f7daf17b12b8",
+    "url": "/hrportal/static/js/20.9b46702c.chunk.js"
+  },
+  {
+    "revision": "78c0c235d0c82334dc2b",
+    "url": "/hrportal/static/js/21.c98c7c12.chunk.js"
+  },
+  {
+    "revision": "70e5948219503f6bb629",
+    "url": "/hrportal/static/js/22.4a9047d2.chunk.js"
+  },
+  {
+    "revision": "f149e45156590db1cee8",
+    "url": "/hrportal/static/js/23.29934b94.chunk.js"
+  },
+  {
+    "revision": "ffdd5f8d52b5db4e286c",
+    "url": "/hrportal/static/js/24.dfb9af17.chunk.js"
+  },
+  {
+    "revision": "5ef13b9c1193e061a4e0",
+    "url": "/hrportal/static/js/25.99fb893d.chunk.js"
+  },
+  {
+    "revision": "274f10df6853fde1e014",
+    "url": "/hrportal/static/js/26.2727ce32.chunk.js"
+  },
+  {
+    "revision": "031553198c0e78bb51ae",
+    "url": "/hrportal/static/js/27.610c8318.chunk.js"
+  },
+  {
+    "revision": "e6ca01f5b07c251d61f6",
+    "url": "/hrportal/static/js/28.485a6df2.chunk.js"
+  },
+  {
+    "revision": "1a567a0cc594fc65f18e",
+    "url": "/hrportal/static/js/29.20664cdf.chunk.js"
+  },
+  {
+    "revision": "eeeb3d9c85dbc8cc7c06",
+    "url": "/hrportal/static/js/3.f37235c2.chunk.js"
+  },
+  {
+    "revision": "3c892f9f80c22cec7f94",
+    "url": "/hrportal/static/js/30.08ade498.chunk.js"
+  },
+  {
+    "revision": "2124359eb04965515468",
+    "url": "/hrportal/static/js/31.87f94a18.chunk.js"
+  },
+  {
+    "revision": "7e30c09419c1e3604764",
+    "url": "/hrportal/static/js/32.1d4b815c.chunk.js"
+  },
+  {
+    "revision": "b1ef3f17b07c53db96ac",
+    "url": "/hrportal/static/js/33.b0d103b9.chunk.js"
+  },
+  {
+    "revision": "2ad8e2b3973c967818e3",
+    "url": "/hrportal/static/js/34.63989129.chunk.js"
+  },
+  {
+    "revision": "8d7e65b3743789313d4b",
+    "url": "/hrportal/static/js/35.f51e68dc.chunk.js"
+  },
+  {
+    "revision": "e97f2a3bc71e576048ce",
+    "url": "/hrportal/static/js/36.d85d8675.chunk.js"
+  },
+  {
+    "revision": "6556db706ecc94a6709a",
+    "url": "/hrportal/static/js/37.6ef79371.chunk.js"
+  },
+  {
+    "revision": "c71595783aba32dd1f28",
+    "url": "/hrportal/static/js/38.d8b4179a.chunk.js"
+  },
+  {
+    "revision": "12fc99fda2ae9993bdf2",
+    "url": "/hrportal/static/js/39.2e5db1f6.chunk.js"
+  },
+  {
+    "revision": "391167dc8d7f137133f5",
+    "url": "/hrportal/static/js/4.09b6a67a.chunk.js"
+  },
+  {
+    "revision": "af5e888e7752ba16f685",
+    "url": "/hrportal/static/js/40.a99bd8b4.chunk.js"
+  },
+  {
+    "revision": "cdec89196d8acc676649",
+    "url": "/hrportal/static/js/41.bf16f96f.chunk.js"
+  },
+  {
+    "revision": "e4f218254b5612bd4b42",
+    "url": "/hrportal/static/js/42.fe12e70e.chunk.js"
+  },
+  {
+    "revision": "b161b616cc9c2adc251b",
+    "url": "/hrportal/static/js/43.8bd9a543.chunk.js"
+  },
+  {
+    "revision": "c2f939194ae34657b0c9",
+    "url": "/hrportal/static/js/44.1d553c3f.chunk.js"
+  },
+  {
+    "revision": "a32acce34e044f7b27ff",
+    "url": "/hrportal/static/js/45.81240fd1.chunk.js"
+  },
+  {
+    "revision": "54fb156f4147ef929fe0",
+    "url": "/hrportal/static/js/46.73df50d7.chunk.js"
+  },
+  {
+    "revision": "5813154901cce004d184",
+    "url": "/hrportal/static/js/47.34cd51e5.chunk.js"
+  },
+  {
+    "revision": "651b0c7a88efc74d98f4",
+    "url": "/hrportal/static/js/48.ae6c7b38.chunk.js"
+  },
+  {
+    "revision": "732ada573968706862f9",
+    "url": "/hrportal/static/js/49.1b1d18e7.chunk.js"
+  },
+  {
+    "revision": "4d2e674f7d4c3a3529a5",
+    "url": "/hrportal/static/js/50.2a05b57e.chunk.js"
+  },
+  {
+    "revision": "e90c4ba4054ee3969f3e",
+    "url": "/hrportal/static/js/51.a5ca3ec5.chunk.js"
+  },
+  {
+    "revision": "622bb8ea4509c27cc184",
+    "url": "/hrportal/static/js/52.3742b78b.chunk.js"
+  },
+  {
+    "revision": "30bc75ae12d636335bc7",
+    "url": "/hrportal/static/js/53.4ae79bf0.chunk.js"
+  },
+  {
+    "revision": "be999c5a6c0a9072591c",
+    "url": "/hrportal/static/js/54.1e33d4b2.chunk.js"
+  },
+  {
+    "revision": "bfd20e852bbf7cbdd4b4",
+    "url": "/hrportal/static/js/55.9ed3e0b8.chunk.js"
+  },
+  {
+    "revision": "062e587511a1a24b784e",
+    "url": "/hrportal/static/js/56.c50cd4e5.chunk.js"
+  },
+  {
+    "revision": "2e90b69975912412a578",
+    "url": "/hrportal/static/js/57.228bd9e7.chunk.js"
+  },
+  {
+    "revision": "6f58e74a2c5642e8be94",
+    "url": "/hrportal/static/js/58.7f4e8a87.chunk.js"
+  },
+  {
+    "revision": "4118fa14705895adacf8",
+    "url": "/hrportal/static/js/59.2309b499.chunk.js"
+  },
+  {
+    "revision": "07645b1336ccd8bf6029",
+    "url": "/hrportal/static/js/60.85c2f5c2.chunk.js"
+  },
+  {
+    "revision": "ef6a16214a0c2e7785d9",
+    "url": "/hrportal/static/js/61.5e5c09b8.chunk.js"
+  },
+  {
+    "revision": "901f63a044e614abc817",
+    "url": "/hrportal/static/js/7.131c7a7d.chunk.js"
+  },
+  {
+    "revision": "1ea1b8432446b5d25d232d43876985e9",
+    "url": "/hrportal/static/js/7.131c7a7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef958fc0340bb122ad1c",
+    "url": "/hrportal/static/js/8.56159a72.chunk.js"
+  },
+  {
+    "revision": "5c9b10f97fb4ef33b40be2abacc4f956",
+    "url": "/hrportal/static/js/8.56159a72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fc6493622362f28431e",
+    "url": "/hrportal/static/js/9.cc9ae0ad.chunk.js"
+  },
+  {
+    "revision": "58250c0771a1e35051d7",
+    "url": "/hrportal/static/js/main.9e9af2ca.chunk.js"
+  },
+  {
+    "revision": "9c18a16b9b2b7ded69a0",
+    "url": "/hrportal/static/js/runtime-main.b1a5b8fa.js"
+  },
+  {
+    "revision": "b9eb3a03009d618aa53a0ca8eaa6ab4b",
+    "url": "/hrportal/static/media/Cairo-SemiBold.b9eb3a03.ttf"
+  },
+  {
+    "revision": "645fb73fd6f7faacd47134ef72a69b93",
+    "url": "/hrportal/static/media/hr-logo.645fb73f.png"
+  }
+]);
